ba45a2c7fa40e8d029df00f6e5fd7bb9a92430fe
Idealm99 <skyblack156@gmail.com>
2024-10-11 17:19:25 +0900

    추가 수정

--- Boston_regression.R
+++ Boston_regression.R
@@ -3,15 +3,14 @@ data("Boston")
 mydata <- Boston
 head(mydata)
 
-# 1. 모형 적합
-model <- lm(medv ~ crim+zn+indus, data = mydata)
+model <- lm(medv ~ crim+tax+chas+ptratio+rm+dis+rad+lstat, data = mydata)
 summary(model)
 
-# 2. 예측값 생성
+
 predicted_values <- predict(model, newdata = Boston)
 
 # MSE
-# 실제값과 예측값
+
 actual_values <- Boston$medv
 
 # MSE (Mean Squared Error)
@@ -26,11 +25,9 @@ rmse
 mae <- mean(abs(actual_values - predicted_values))
 mae
 
-# 3. 모형 진단
-# 레이아웃 설정: 2x2 그리드 (한 화면에 4개의 그래프 표시)
+
 par(mfrow = c(2, 2))
 
-# 회귀모형 진단 플롯 생성
 plot(model)
 
 
